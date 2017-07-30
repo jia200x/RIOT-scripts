3ac8e35070e741a80de3e0c527854655d584ddf2
ramirez <jialamos@uc.cl>
2017-07-30 19:38:13 -0400

    move toolchain url to variable

--- RIOT_Tutorial_x86_64.sh
+++ RIOT_Tutorial_x86_64.sh
@@ -1,3 +1,4 @@
+TOOLCHAIN_URL=https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2?product=GNU%20ARM%20Embedded%20Toolchain,64-bit,,Linux,6-2017-q2-update
 CURR_DIR=$(pwd)
 mkdir tmp
 cd tmp
@@ -29,7 +30,7 @@ sudo make install
 
 #Install ARM Toolchain
 cd $RIOT_TMP
-wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2?product=GNU%20ARM%20Embedded%20Toolchain,64-bit,,Linux,6-2017-q2-update -O gcc.tar.bz2
+wget $TOOLCHAIN_URL -O gcc.tar.bz2
 mkdir ~/opt
 tar -xf gcc.tar.bz2 -C ~/opt/
 cd ~/opt/gcc-*
