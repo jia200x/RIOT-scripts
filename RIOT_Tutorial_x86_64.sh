27e2a30901e2ded9cee303d8b6189e93be29acc4
ramirez <jialamos@uc.cl>
2017-07-30 19:34:50 -0400

    add comments

--- RIOT_Tutorial_x86_64.sh
+++ RIOT_Tutorial_x86_64.sh
@@ -10,11 +10,13 @@ sudo apt-get install git -y
 #Clone RIOT Tutorials
 git clone --recursive https://github.com/RIOT-OS/Tutorials
 
+#Install dependencies
 sudo apt-get install build-essential pkg-config autoconf automake libtool libusb-dev libusb-1.0-0-dev libhidapi-dev -y
 sudo apt-get install python-pip -y
 sudo apt-get install gcc-multilib -y
 pip install pyserial -y
 
+#Install OpenOCD
 cd $RIOT_TMP
 git clone git://git.code.sf.net/p/openocd/code openocd
 cd openocd/
@@ -22,6 +24,8 @@ cd openocd/
 ./configure
 make
 sudo make install
+
+#Install ARM Toolchain
 cd $RIOT_TMP
 wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2?product=GNU%20ARM%20Embedded%20Toolchain,64-bit,,Linux,6-2017-q2-update -O gcc.tar.bz2
 mkdir ~/opt
@@ -31,13 +35,16 @@ cd bin/
 sudo su root -c 'echo "export PATH=\$PATH:$(pwd)" >> /etc/profile'
 source /etc/profile && export PATH
 
+#Add udev rule for flashing samr21
 echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="plugdev"'     | sudo tee -a /etc/udev/rules.d/99-usb.rules
 sudo service udev restart
 
 
+#Remove tmp dir
 cd $CURR_DIR
 rm -rf $RIOT_TMP
 
+#Run default example
 cd $CURR_DIR/Tutorials/RIOT/examples/default
 make BOARD=samr21-xpro all flash
 sudo make BOARD=samr21-xpro term
